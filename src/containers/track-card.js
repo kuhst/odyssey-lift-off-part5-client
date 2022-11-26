b793046072d0027008073edae87995f8889c08d0
Oleg Kuharchuk <73923734+kuhst@users.noreply.github.com>
2022-11-26 08:32:00 +0300

    replace "length" to "durationInSeconds"

--- src/containers/track-card.js
+++ src/containers/track-card.js
@@ -27,14 +27,10 @@ const INCREMENT_TRACK_VIEWS = gql`
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount, id } = track;
+  const { title, thumbnail, author, durationInSeconds, modulesCount, id } = track;
 
   const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
     variables: { incrementTrackViewsId: id },
-    // to observe what the mutation response returns
-    onCompleted: (data) => {
-      console.log(data);
-    },
   });
 
   return (
@@ -50,7 +46,7 @@ const TrackCard = ({ track }) => {
             <AuthorAndTrack>
               <AuthorName>{author.name}</AuthorName>
               <TrackLength>
-                {modulesCount} modules - {humanReadableTimeFromSeconds(length)}
+                {modulesCount} modules - {humanReadableTimeFromSeconds(durationInSeconds)}
               </TrackLength>
             </AuthorAndTrack>
           </CardFooter>
